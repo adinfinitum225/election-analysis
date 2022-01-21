3592ef8f74c8098b4c076c851463b198f09cf85d
Zach Wells <zach.13.wells@gmail.com>
2022-01-21 14:05:19 -0600

    completed 3.5.6

--- PyPoll.py
+++ PyPoll.py
@@ -20,6 +20,11 @@ candidate_options = []
 # Initialize the dicionary to count votes
 candidate_votes = {}
 
+# Winning candidate and winning count tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 # Open the election results and read the file
 with open(file_to_load) as election_data:
 
@@ -48,6 +53,29 @@ with open(file_to_load) as election_data:
             candidate_votes[candidate_name] = 0
 
         candidate_votes[candidate_name] += 1
+
+    for candidate_name in candidate_votes:
+        # Retrieve vote count of candidate
+        votes = candidate_votes[candidate_name]
+        # calculate percentage of votes
+        vote_percentage = float(votes) / float(total_votes) * 100
+        # Print the name and percentage of votes
+        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+        # Determine winning vote count and candidate
+        # Determine if the votes is greater than the winning count.
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            winning_count = votes
+            winning_percentage = vote_percentage
+            winning_candidate = candidate_name
+
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
+    print(winning_candidate_summary)
 
 
 # 1. The total number of votes cast
