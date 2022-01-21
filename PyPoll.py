b71ef33892b2c0f480157ac702fa5c5368bc431c
Zach Wells <zach.13.wells@gmail.com>
2022-01-21 13:38:59 -0600

    Completed 3.5.4

--- PyPoll.py
+++ PyPoll.py
@@ -11,6 +11,15 @@ with open(file_to_save) as election_analysis:
 
     print(election_analysis)
 
+# Initilize total votes variable and set to zero
+total_votes = 0
+
+# Initialize a list for the candidate names
+candidate_options = []
+
+# Initialize the dicionary to count votes
+candidate_votes = {}
+
 # Open the election results and read the file
 with open(file_to_load) as election_data:
 
@@ -23,8 +32,33 @@ with open(file_to_load) as election_data:
     headers = next(file_reader)
     print(headers)
 
+    for row in file_reader:
+        #Add to total vote count
+        total_votes += 1
+
+        # Get the candidate name from each row
+        candidate_name = row[2]
+
+        if candidate_name not in candidate_options:
+
+            # Add the name to the options list
+            candidate_options.append(candidate_name)
+
+            # Begin tracking that candidate's votes
+            candidate_votes[candidate_name] = 0
+
+        candidate_votes[candidate_name] += 1
+
+
 # 1. The total number of votes cast
+print(total_votes)
+
 # 2. A complete list of candidates who received votes
+print(candidate_options)
+
+# 3. Print candidate vote dictionary
+print(candidate_votes)
+
 # 3. The percentage of votes each candidate won
 # 4. The total number of votes each candidate won
 # 5. The winner of the election based on popular vote
